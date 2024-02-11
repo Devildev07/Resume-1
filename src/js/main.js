7c0e03505cc74f22178d318261c40d39ed3933e2
Dewanshu Singh <dewanshusingh21@gmail.com>
2024-02-11 21:19:55 +0530

    resume 1 done

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,15 @@
-console.log("resume 1");
+const themeToggle = document.querySelector("#themeBtn");
+
+themeToggle.addEventListener("click", function () {
+  const html = document.documentElement;
+  html.classList.toggle("dark");
+  const currentTheme = html.classList.contains("dark") ? "dark" : "";
+  localStorage.setItem("theme", currentTheme);
+});
+
+// Check localStorage for saved theme
+const savedTheme = localStorage.getItem("theme");
+console.log(savedTheme);
+if (savedTheme) {
+  document.documentElement.classList.add(savedTheme);
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
+const plugin = require("tailwindcss/plugin");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addComponents }) {
+      addComponents({
+        ".btn": {
+          padding: ".5rem 1rem",
+          borderRadius: ".25rem",
+          fontWeight: "600",
+        },
+        ".btn-blue": {
+          // backgroundColor: "#3490dc",
+          color: "#fff",
+          "&:hover": {
+            // backgroundColor: '#2779bd'
+          },
+        },
+      });
+    }),
+  ],
+
+  darkMode: "class",
+  
 };
